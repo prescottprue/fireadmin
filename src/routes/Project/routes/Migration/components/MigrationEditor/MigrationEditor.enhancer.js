63922505871d6a5e73d0404418d8452fb2423149
prescottprue <sprue.dev@gmail.com>
2017-12-20 02:19:11 -0800

    Fix issue with Firepad throwing error when leaving the page.

--- src/routes/Project/routes/Migration/components/MigrationEditor/MigrationEditor.enhancer.js
+++ src/routes/Project/routes/Migration/components/MigrationEditor/MigrationEditor.enhancer.js
@@ -1,14 +1,22 @@
 import { compose } from 'redux'
 import { withHandlers } from 'recompose'
 import { withFirebase } from 'react-redux-firebase'
+import { invoke } from 'lodash'
 import { firebasePaths } from 'constants'
 import CodeMirror from 'codemirror'
 import { getCodeMirror } from './MigrationEditor.utils'
+
+let firepadEditor
 
 export default compose(
   withFirebase,
   withHandlers({
     setupEditor: ({ firebase, params: { projectId } }) => el => {
+      // Called when unmounting
+      if (el === null) {
+        // Dispose of firepad instance
+        return invoke(firepadEditor, 'dispose')
+      }
       const doc = new CodeMirror.Doc('', 'javascript')
       const editor = getCodeMirror(el, doc)
       editor.setOption('theme', 'monokai')
@@ -18,7 +26,7 @@ export default compose(
         defaultText: `// run custom code here\n// const someData = await admin.firebase.ref('some/path').once('value')`
       }
       try {
-        Firepad.fromCodeMirror(fbRef, editor, settings)
+        firepadEditor = Firepad.fromCodeMirror(fbRef, editor, settings)
       } catch (err) {
         console.error('It threw :(') // eslint-disable-line
       }
