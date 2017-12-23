5430d6e582b6b31289bcc77c713cade70d45e3e2
Scott Prue <scott@residenetwork.com>
2017-12-22 21:18:30 -0800

    StackDriverErrorReporter added (currently loaded through script tag).

---
+++ src/utils/errorHandler.js
@@ -0,0 +1,25 @@
+import { firebase, googleApis } from '../config'
+import { version } from '../../package.json'
+
+const environment = process.env.NODE_ENV
+
+let errorHandler
+
+export function init() {
+  if (environment === 'production') {
+    window.addEventListener('DOMContentLoaded', () => {
+      errorHandler = new window.StackdriverErrorReporter()
+      errorHandler.start({
+        key: googleApis.apiKey,
+        projectId: firebase.projectId,
+        service: 'fireadmin-site',
+        version
+      })
+    })
+  } else {
+    errorHandler = console.error // eslint-disable-line no-console
+  }
+  return errorHandler
+}
+
+export default errorHandler
