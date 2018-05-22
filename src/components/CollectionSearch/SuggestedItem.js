d2c709d3ee50616a6ddeb5140e98317a4ac5783b
Scott Prue <scott@residebrokerage.com>
2018-05-22 02:50:47 -0700

    Add step count to action search results.

--- src/components/CollectionSearch/SuggestedItem.js
+++ src/components/CollectionSearch/SuggestedItem.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { size } from 'lodash'
 import { MenuItem } from 'material-ui/Menu'
 import { Highlight } from 'react-instantsearch/dom'
 
 const SuggestedUser = ({ hit, onClick }) => (
   <MenuItem style={{ marginTop: '10px' }} onClick={() => onClick(hit)}>
-    <span className="hit-name">
-      <Highlight attributeName="name" hit={hit} />
-    </span>
+    <div className="flex-column">
+      <span className="hit-name">
+        <Highlight attributeName="name" hit={hit} />
+      </span>
+      <div
+        className="flex-column"
+        style={{ fontSize: '.75rem', color: '#757575' }}>
+        <span>
+          Steps: <strong>{size(hit.steps)}</strong>
+        </span>
+      </div>
+    </div>
   </MenuItem>
 )
 
