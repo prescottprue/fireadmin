71077609bea8fdea87e6065f131659941c8eae66
prescottprue <sprue.dev@gmail.com>
2018-07-02 02:27:51 -0700

    Move e2e tests to their own repo [ci skip]

--- test/e2e/querySelectors/login.js
+++
@@ -1,8 +0,0 @@
-export default {
-  googleAuthButton: '#google-auth-button',
-  signUp: '#sign-in',
-  emailField: '#identifierId',
-  nextButton: '#identifierNext',
-  passwordField: '#password',
-  passwordNextButton: '#passwordNext',
-}
