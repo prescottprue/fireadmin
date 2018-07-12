6d42764b2144e48d2fe64803eb44cdace57c0156
Scott Prue <scott@residebrokerage.com>
2018-07-11 18:16:11 -0700

    Gitlab deployment
    Add deploy steps to gitlab

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -2,6 +2,7 @@ describe('Home', () => {
   beforeEach(() => {
     cy.visit('/')
   })
+
   it('Goes to login page', () => {
     cy.get('[data-test=sign-in]').click()
     cy.get('[data-test=google-auth-button]').should('exist')
