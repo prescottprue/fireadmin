c953a17c348b432874de62882611b71888c98281
Scott Prue <scott@residebrokerage.com>
2018-07-16 18:37:55 -0700

    feat(tests): add a test to confirm that logging in programtically works

--- cypress/integration/projects_spec.js
+++ cypress/integration/projects_spec.js
@@ -4,6 +4,10 @@ describe('Projects Page', () => {
     cy.visit('/projects')
   })
 
+  it('Logs in programatically', () => {
+    cy.url().should('include', '/projects')
+  })
+
   it('Supports adding a new project', () => {
     cy.get('[data-test=new-project-tile]').click()
   })
