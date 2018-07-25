bb5e1c77f22df9aef7537e68ea98572284e23157
Scott Prue <scott@residebrokerage.com>
2018-07-25 16:50:52 -0700

    Start stubbing of requests in projects test.

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,10 +1,12 @@
+import { createSelector } from '../utils'
+
 describe('Home', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('Goes to login page', () => {
-    cy.get('[data-test=sign-in]').click()
-    cy.get('[data-test=google-auth-button]').should('exist')
+    cy.get(createSelector('sign-in')).click()
+    cy.get(createSelector('google-auth-button')).should('exist')
   })
 })
--- cypress/integration/projects_spec.js
+++ cypress/integration/projects_spec.js
@@ -1,23 +1,59 @@
 import { createSelector } from '../utils'
 
 describe('Projects Page', () => {
+  let open // eslint-disable-line no-unused-vars
+  // Setup before tests including creating a server to listen for external
+  // requests
   before(() => {
+    // Create a server to listen to requests sent out to Google Auth and Firestore
+    cy.server()
+      .route('POST', /identitytoolkit\/v3\/relyingparty\/getAccountInfo\//)
+      .as('getGoogleAccountInfo')
+      .route('POST', /identitytoolkit\/v3\/relyingparty\/verifyCustomToken\//)
+      .as('verifyCustomFirebaseToken')
+      .route('POST', /google.firestore.v1beta1.Firestore\/Write\//)
+      .as('addProject')
+      .route('POST', /google.firestore.v1beta1.Firestore\/Listen\//)
+      .as('listenForProjects')
+      .route('GET', /google.firestore.v1beta1.Firestore\/Listen\//)
+      .as('getProjectData')
+      .window()
+      .then(win => {
+        // Create a spy on the servers onOpen event so we can later expect
+        // it to be called with specific arguments
+        open = cy.spy(cy.state('server').options, 'onOpen')
+        return null
+      })
+    // Go to home page
     cy.visit('/')
+    // Login using custom token
     cy.login()
-    cy.wait(1000)
-    cy.visit('/login')
-    cy.wait(2000)
+    // Go to projects page
+    cy.visit('/projects').then(() =>
+      // Wait for all data requests to complete before proceeding
+      // we use promise all because responses can return at different times
+      Promise.all([
+        cy.wait('@verifyCustomFirebaseToken'),
+        cy.wait('@getGoogleAccountInfo'),
+        cy.wait('@listenForProjects'),
+        cy.wait('@getProjectData')
+      ])
+    )
   })
 
   describe('Add Project', () => {
     it('creates project when provided a valid name', () => {
       const newProjectTitle = 'Test project'
-      cy.get(createSelector('new-project-tile')).click()
+      cy.get(createSelector('new-project-tile'), { timeout: 8000 }).click()
+      // Type name of new project into input
       cy.get(createSelector('new-project-name'))
         .find('input')
         .type(newProjectTitle)
+      // Click on the new project button
       cy.get(createSelector('new-project-create-button')).click()
-      // confirm first project tile has title passed to new project input
+      // Wait for request to Firebase to add project to return
+      cy.wait('@addProject')
+      // Confirm first project tile has title passed to new project input
       cy.get(createSelector('project-tile-name'))
         .first()
         .should('have.text', newProjectTitle)
@@ -30,8 +66,9 @@ describe('Projects Page', () => {
       cy.get(createSelector('project-tile-more'))
         .first()
         .click()
+      // Click on the "delete" option in the more options
       cy.get(createSelector('project-tile-delete')).click()
-      // Confirm that new project is not available
+      // Confirm that new project is no longer available
       cy.get(createSelector('new-project-name')).should('not.exist')
     })
   })
