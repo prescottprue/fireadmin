74328d2be2d5e915740819ae9d003680be701fc9
Scott <prescottprue@users.noreply.github.com>
2018-08-03 01:26:50 -0700

    v0.5.6 (#46)
    * feat(projects): set default roles when creating a project

--- src/constants.js
+++ src/constants.js
@@ -47,4 +47,47 @@ export const firebasePaths = {
   actionRunnerResponses: 'responses/actionRunner'
 }
 
+export const defaultRoles = {
+  owner: {
+    name: 'Owner',
+    permissions: {
+      read: {
+        environments: true,
+        members: true,
+        permissions: true,
+        roles: true
+      },
+      update: {
+        environments: true,
+        members: true,
+        permissions: true,
+        roles: true
+      },
+      delete: {
+        environments: true,
+        members: true,
+        permissions: true,
+        roles: true
+      },
+      create: {
+        environments: true,
+        members: true,
+        permissions: true,
+        roles: true
+      }
+    }
+  },
+  editor: {
+    name: 'Editor',
+    permissions: {
+      read: { environments: true },
+      update: { environments: true },
+      create: { environments: true }
+    }
+  },
+  viewer: {
+    permissions: { read: { environments: true } }
+  }
+}
+
 export default { ...paths, ...formNames }
--- src/routes/Projects/components/ProjectsPage/ProjectsPage.handlers.js
+++ src/routes/Projects/components/ProjectsPage/ProjectsPage.handlers.js
@@ -1,47 +1,67 @@
-import { LIST_PATH } from 'constants'
+import { LIST_PATH, defaultRoles } from 'constants'
 import { triggerAnalyticsEvent } from 'utils/analytics'
 
-export const addProject = props => async newInstance => {
-  const { firestore, firebase, uid, showError } = props
-  if (!uid) {
-    return showError('You must be logged in to create a project')
-  }
-  try {
-    props.toggleDialog()
-    const res = await firestore.add(
-      { collection: 'projects' },
-      {
-        ...newInstance,
-        createdBy: uid,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp()
-      }
-    )
-    props.showSuccess('Project added successfully')
-    triggerAnalyticsEvent('createProject')
-    return res
-  } catch (err) {
-    showError(err.message || 'Could not add project')
-    throw err
+/**
+ * Handler for adding a project
+ * @param {Object} props - component props
+ */
+export function addProject(props) {
+  return async newInstance => {
+    const { firestore, firebase, uid, showError } = props
+    if (!uid) {
+      return showError('You must be logged in to create a project')
+    }
+    try {
+      props.toggleDialog()
+      const res = await firestore.add(
+        { collection: 'projects' },
+        {
+          ...newInstance,
+          createdBy: uid,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+          permissions: {
+            [uid]: {
+              role: 'owner',
+              updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+            }
+          },
+          roles: defaultRoles
+        }
+      )
+      props.showSuccess('Project added successfully')
+      triggerAnalyticsEvent('createProject')
+      return res
+    } catch (err) {
+      showError(err.message || 'Could not add project')
+      throw err
+    }
   }
 }
 
-export const deleteProject = props => async projectId => {
-  const { firestore, showError, showSuccess } = props
-  try {
-    await firestore.delete({ collection: 'projects', doc: projectId })
-    showSuccess('Project deleted successfully')
-    triggerAnalyticsEvent('deleteProject', { projectId })
-  } catch (err) {
-    console.error('Error:', err) // eslint-disable-line no-console
-    showError(err.message || 'Could not add project')
+/**
+ * Handler for deleting a project
+ * @param {Object} props - component props
+ */
+export function deleteProject(props) {
+  return async projectId => {
+    const { firestore, showError, showSuccess } = props
+    try {
+      await firestore.delete({ collection: 'projects', doc: projectId })
+      showSuccess('Project deleted successfully')
+      triggerAnalyticsEvent('deleteProject', { projectId })
+    } catch (err) {
+      console.error('Error deleting project:', err) // eslint-disable-line no-console
+      showError(err.message || 'Error deleting project')
+    }
   }
 }
 
-export const goToProject = ({ router }) => projectId => {
-  router.push(`${LIST_PATH}/${projectId}`)
-}
-
-export const goToCollaborator = ({ router, showError }) => userId => {
-  showError('User pages are not yet supported!')
-  // router.push(`${USERS_PATH}/${userId}`)
+/**
+ * Handler for navigating to a project
+ * @param {Object} props - component props
+ */
+export function goToProject({ router }) {
+  return projectId => {
+    router.push(`${LIST_PATH}/${projectId}`)
+  }
 }
