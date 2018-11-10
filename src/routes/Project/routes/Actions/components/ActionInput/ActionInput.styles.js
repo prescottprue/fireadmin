9b4071ed682bbfef356d5ac2c4b2af622f3f4c3e
Scott <prescottprue@users.noreply.github.com>
2018-11-10 01:51:31 -0800

    v0.6.6 (#62)
    * fix(actionRunner): update styling of environments and inputs sections of action runner
    * fix(deps): update @material-ui/core to ^3.4.0 as well as react + react-dom to ^16.6.1

---
+++ src/routes/Project/routes/Actions/components/ActionInput/ActionInput.styles.js
@@ -0,0 +1,9 @@
+export default theme => ({
+  input: {
+    margin: theme.spacing.unit
+  },
+  field: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  }
+})
--- src/routes/Project/routes/Actions/components/ActionInputs/index.js
+++
@@ -1,4 +0,0 @@
-import ActionInputs from './ActionInputs'
-import enhancer from './ActionInputs.enhancer'
-
-export default enhancer(ActionInputs)
