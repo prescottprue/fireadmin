10f42e3d03d6ad72df956d76d2699353f00a6bbe
prescottprue <sprue.dev@gmail.com>
2019-03-01 17:13:29 -0800

    Fix size limits to match new sizes

--- .size-limit.js
+++ .size-limit.js
@@ -1,22 +1,15 @@
 module.exports = [
   {
     path: "dist/main.*.js",
-    limit: "1.5 MB",
+    limit: "1.1 MB",
     name: "Main bundle",
     webpack: false,
     gzip: false
   },
   {
     path: "dist/vendor.*.js",
-    limit: "310 KB",
+    limit: "850 KB",
     name: "Vendor bundle",
-    gzip: false,
-    webpack: false
-  },
-  {
-    path: "dist/*.*.js",
-    limit: "310 KB",
-    name: "Other bundles",
     gzip: false,
     webpack: false
   }
