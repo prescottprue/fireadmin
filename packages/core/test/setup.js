8ca14a812255ff3ae7940c0bcc6833d88041c0c0
prescottprue <sprue.dev@gmail.com>
2019-03-30 01:32:10 -0700

    First test running

---
+++ packages/core/test/setup.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+
+global.chai = chai
+global.expect = chai.expect
+global.assert = chai.assert
---
+++ packages/core/test/unit/Project.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import Project from '../../src/Project'
+
+describe('Projects', () => {
+  it('creates a project', () => {
+    expect(Project).to.exist
+  })
+})
