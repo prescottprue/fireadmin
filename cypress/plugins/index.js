20f2b39d440adfd2b5d8cb73aa12a474cba1ce50
Scott Prue <scott@residebrokerage.com>
2020-03-13 23:58:00 -0700

    Add back log-to-output

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,6 +14,20 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   require('cypress-plugin-retries/lib/plugin')(on)
+  require('cypress-log-to-output').install(on, (type, event) => {
+    // return true or false from this plugin to control if the event is logged
+    // `type` is either `console` or `browser`
+    // if `type` is `browser`, `event` is an object of the type `LogEntry`:
+    //  https://chromedevtools.github.io/devtools-protocol/tot/Log#type-LogEntry
+    // if `type` is `console`, `event` is an object of the type passed to `Runtime.consoleAPICalled`:
+    //  https://chromedevtools.github.io/devtools-protocol/tot/Runtime#event-consoleAPICalled
+
+    // if (event.level === 'error' || event.type === 'error') {
+    //   return true
+    // }
+
+    return true
+  })
   // Extends with config from .firebaserc
   return cypressFirebasePlugin(on, config, admin)
 }
