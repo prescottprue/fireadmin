193887e2233882dfc1e7a7d8c7976127e352e758
Scott Prue <scott@residebrokerage.com>
2020-03-13 19:36:56 -0700

    Improve projects page tests. Remove cypress-log-to-output

--- cypress/integration/ProjectsPage.spec.js
+++ cypress/integration/ProjectsPage.spec.js
@@ -26,11 +26,12 @@ describe('Projects Page', () => {
   })
 
   describe('Delete Project', () => {
-    beforeEach(() => {
+    before(() => {
       const fakeProject = {
         name: 'test delete project',
         collaborators: { [Cypress.env('TEST_UID')]: true }
       }
+      cy.callFirestore('delete', 'projects')
       cy.callFirestore('set', 'projects/test-delete-project', fakeProject)
     })
 
@@ -59,16 +60,33 @@ describe('Projects Page', () => {
       name: 'collab project 2',
       collaborators: { [Cypress.env('TEST_UID')]: true }
     }
+    const ownedProject = {
+      name: 'owned an collab project',
+      createdBy: Cypress.env('TEST_UID'),
+      collaborators: { [Cypress.env('TEST_UID')]: true }
+    }
     before(() => {
+      cy.callFirestore('delete', 'projects')
       cy.callFirestore('set', 'projects/collab-project-1', fakeProject)
       cy.callFirestore('set', 'projects/collab-project-2', fakeProject2)
+      cy.callFirestore('set', 'projects/owned-project-1', ownedProject)
+    })
+
+    it('shows projects which are created by the current user', () => {
+      // Confirm first project tile has title passed to new project input
+      cy.get(createIdSelector('owned-project-1')).should('exist')
     })
 
     it('shows projects which have the user as a collaborator', () => {
       // Confirm first project tile has title passed to new project input
+      cy.get(createIdSelector('collab-project-1')).should('exist')
+      cy.get(createIdSelector('collab-project-2')).should('exist')
+    })
+
+    it('does not display the same project twice (even is creator is also collaborator)', () => {
       cy.get(createSelector('project-tile-name'))
         .its('length')
-        .should('be.gte', 2)
+        .should('be', 3)
     })
   })
 })
