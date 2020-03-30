d504204517707b2cd1ef15353ee62aee072caf5b
Scott Prue <scott@residebrokerage.com>
2020-03-29 20:10:56 -0700

    Fix eslint within docs

--- docs/.eslintrc.js
+++ docs/.eslintrc.js
@@ -1,3 +1,43 @@
+// TODO: Switch back to "extends: '../.eslintrc.js'" once react-app is removed from top level
 module.exports = {
-  extends: '../.eslintrc.js',
+  'extends': ['prettier', 'prettier/react'],
+  root: true,
+  parser: 'babel-eslint',
+  plugins: ['import', 'babel', 'react', 'react-hooks', 'prettier'],
+  settings: {
+    react: {
+      version: '16.11'
+    },
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', '/']
+      }
+    }
+  },
+  rules: {
+    semi: [
+      2, 'never'
+    ],
+    'no-console': 'error',
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': 0,
+    'import/no-named-as-default': 0,
+    'no-return-await': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        semi: false,
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false
+      }
+    ]
+  }
 }
