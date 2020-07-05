eaf92383da0584131b6f5ece85bcaa17a1085a54
Scott Prue <sprue.dev@gmail.com>
2020-07-05 16:28:38 -0400

    Change again

--- functions/src/indexUser/index.js
+++ functions/src/indexUser/index.js
@@ -4,7 +4,6 @@ import { createIndexFunc } from '../utils/search'
 
 // Updates the search index when users are created or displayName is updated
 // another comment
-// another one
 export default functions.firestore.document('/users/{userId}').onWrite(
   createIndexFunc({
     indexName: 'users',
