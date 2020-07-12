81ea3258abdf558821f8e53a1a8551b8c89d22a5
Scott Prue <sprue.dev@gmail.com>
2020-07-12 16:03:26 -0400

    Add log for firebase config

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -22,9 +22,11 @@ const firebaseConfig = {
 
 // Enable Real Time Database emulator if environment variable is set
 if (process.env.REACT_APP_FIREBASE_DATABASE_EMULATOR_HOST) {
-  firebaseConfig.databaseURL = `http://${process.env.REACT_APP_FIREBASE_DATABASE_EMULATOR_HOST}?ns=${firebaseConfig.projectId}`
+  firebaseConfig.databaseURL = `http://${process.env.REACT_APP_FIREBASE_DATABASE_EMULATOR_HOST}?ns=${process.env.REACT_APP_FIREBASE_projectId}`
   console.debug(`RTDB emulator enabled: ${firebaseConfig.databaseURL}`) // eslint-disable-line no-console
 }
+
+console.log('-------- client firebase config', firebaseConfig)
 
 function App({ routes }) {
   return (
