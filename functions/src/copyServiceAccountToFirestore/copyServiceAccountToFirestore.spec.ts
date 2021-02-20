89a7c71c6dc4694e769ad3a1d4389d376060966e
Scott Prue <sprue.dev@gmail.com>
2021-02-20 00:42:34 -0500

    Add mock config and switch back node version change

--- functions/src/copyServiceAccountToFirestore/copyServiceAccountToFirestore.spec.ts
+++ functions/src/copyServiceAccountToFirestore/copyServiceAccountToFirestore.spec.ts
@@ -7,6 +7,12 @@ import { to } from '../utils/async'
 
 const functionsTest = functionsTestLib()
 
+functionsTest.mockConfig({
+  encryption: {
+    password: 'asdf'
+  }
+})
+
 describe('copyServiceAccountToFirestore Firestore Cloud Function (onCreate)', () => {
   let copyServiceAccountToFirestore
   let refStub // eslint-disable-line no-unused-vars
